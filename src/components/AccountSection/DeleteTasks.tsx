f83b3c7a5721b6c7febe82b0a0fb925434c265c2
Shayan-Mst <shnbackup007@gmail.com>
2025-04-12 14:05:22 +0330

    inital

--- src/components/AccountSection/DeleteTasks.tsx
+++ src/components/AccountSection/DeleteTasks.tsx
@@ -22,7 +22,7 @@ const DeleteTasks: React.FC = () => {
         />
       )}
       <button
-        className="mt-auto text-left pt-4 hover:text-rose-600 dark:hover:text-slate-200 transition "
+        className="mt-auto text-center p-4 text-rose-600 hover:text-slate-200 hover:bg-red-700 dark:hover:text-slate-200 transition "
         onClick={() => setIsModalShown(true)}
       >
         Delete all data
